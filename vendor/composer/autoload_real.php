d434374f2b82c35ddba21c53aecf0e503a284b33
Yacine-df <ycn.diaf@gmail.com>
2024-09-26 20:49:49 +0000

    Installing illuminate support laravel package in order to use helper functions all laravel packages uses this package

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,33 +25,33 @@ class ComposerAutoloaderInit6ff18ce784a17e24078c09e982a7d41e
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit6ff18ce784a17e24078c09e982a7d41e', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit6ff18ce784a17e24078c09e982a7d41e', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit6ff18ce784a17e24078c09e982a7d41e::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit6ff18ce784a17e24078c09e982a7d41e::getInitializer($loader));
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit6ff18ce784a17e24078c09e982a7d41e::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire6ff18ce784a17e24078c09e982a7d41e($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire6ff18ce784a17e24078c09e982a7d41e($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
